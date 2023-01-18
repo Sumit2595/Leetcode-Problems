9cf13b800d7107830899ad56ece79bdc4239cfc5
Sumit2595 <72004520+Sumit2595@users.noreply.github.com>
2023-01-18 12:31:00 +0530

    Time: 6 ms (8.15%), Space: 7.8 MB (51.47%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -11,7 +11,13 @@ public:
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n+1,-1);
-        return solve(nums,n,dp);
+        vector<int>dp(n+1,0);
+        dp[1]=nums[0];
+        for(int i=2;i<=n;i++){
+            int op1=nums[i-1]+dp[i-2];
+            int op2=dp[i-1];
+            dp[i]=max(op1,op2);
+        }
+        return dp[n];
     }
 };
