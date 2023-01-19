e33666f2d594484bb87f8f3c473d7060be352a87
Sumit2595 <72004520+Sumit2595@users.noreply.github.com>
2023-01-19 14:42:01 +0530

    Time: 112 ms (92.86%), Space: 68.8 MB (82.14%) - LeetHub

--- 2036-maximum-alternating-subarray-sum/2036-maximum-alternating-subarray-sum.cpp
+++ 2036-maximum-alternating-subarray-sum/2036-maximum-alternating-subarray-sum.cpp
@@ -2,14 +2,19 @@ class Solution {
 public:
     #define ll long long
     long long maximumAlternatingSubarraySum(vector<int>& nums) {
+       
+        return optimized(nums);
+    }
+    ll optimized(vector<int>& nums){
         ll n=nums.size();
-        vector<ll>add(n,0),sub(n,0);
+        ll add=0,sub=0,prevSub=0;
         ll ans=nums[0];
-        add[0]=nums[0];
+        add=nums[0];
         for(int i=1;i<n;i++){
-            sub[i]=add[i-1]-nums[i];
-            add[i]=max<ll>(sub[i-1]+nums[i],1ll * nums[i]);
-            ans=max({ans,add[i],sub[i]});
+            sub=add-nums[i];
+            add=max<ll>(prevSub+nums[i],1ll * nums[i]);
+            ans=max({ans,add,sub});
+            prevSub=sub;
         }
         return ans;
     }
