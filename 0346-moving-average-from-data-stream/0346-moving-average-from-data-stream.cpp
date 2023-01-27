827728543e357599d3d0fdff6fcdf2970b933c06
Sumit2595 <72004520+Sumit2595@users.noreply.github.com>
2023-01-27 12:19:43 +0530

    Time: 31 ms (47.66%), Space: 14.1 MB (39.56%) - LeetHub

---
+++ 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.cpp
@@ -0,0 +1,29 @@
+class MovingAverage {
+public:
+    int sum=0;
+    deque<int>dq;
+    int sz;
+    MovingAverage(int size) {
+        sz=size;
+    }
+    
+    double next(int val) {
+        dq.push_back(val);
+        sum+=val;
+        if(dq.size()<=sz){
+            cout<<sum<<" ";
+        } else{
+            int first=dq.front();
+            dq.pop_front();
+            sum-=first;
+            cout<<sum<<" ";
+        }
+        return sum/(double)dq.size();
+    }
+};
+
+/**
+ * Your MovingAverage object will be instantiated and called as such:
+ * MovingAverage* obj = new MovingAverage(size);
+ * double param_1 = obj->next(val);
+ */
