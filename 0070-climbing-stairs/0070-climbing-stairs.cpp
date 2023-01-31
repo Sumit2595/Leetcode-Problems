4b6da2fb73528274e48128e2b295b8f362269735
Sumit2595 <72004520+Sumit2595@users.noreply.github.com>
2023-01-31 15:30:15 +0530

    Time: 4 ms (16.03%), Space: 6.4 MB (17.00%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     unordered_map<int,int>mp;
-    int solve(int n){
-        if(n<0)
-            return 0;
-        if(n==0)
-            return 1;
-        if(mp.find(n)!=mp.end())
-            return mp[n];
-        return mp[n]=solve(n-1)+solve(n-2);
-    }
     int climbStairs(int n) {
-       return solve(n);
+        if(n<=2)
+            return n;
+        if(mp.find(n)!=mp.end()){
+            return mp[n];
+        }
+        return mp[n]=climbStairs(n-1) + climbStairs(n-2);
     }
 };
