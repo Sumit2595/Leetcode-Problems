add2a6c613facb00389bdeffddb75a67afd4a32c
Sumit2595 <72004520+Sumit2595@users.noreply.github.com>
2023-02-05 23:12:41 +0530

    Time: 37 ms (87.16%), Space: 19.1 MB (50.34%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -6,7 +6,7 @@ public:
         vector<vector<int>>ans;
         int start=nums[0][0],end=nums[0][1];
         for(int i=1;i<n;i++){
-            if(end>=nums[i][0] || end>=nums[i][1]){
+            if(end>=nums[i][0] && end<=nums[i][1] || end>nums[i][1]){
                 end=max(end,nums[i][1]);
             } else {
                 ans.push_back({start,end});
