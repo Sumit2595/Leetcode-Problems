53fe4e0d3e5ec445ec77debce0338288a579cdc7
Sumit2595 <72004520+Sumit2595@users.noreply.github.com>
2023-02-08 00:28:40 +0530

    Time: 19 ms (97.79%), Space: 26.9 MB (98.26%) - LeetHub

--- 0665-non-decreasing-array/0665-non-decreasing-array.cpp
+++ 0665-non-decreasing-array/0665-non-decreasing-array.cpp
@@ -2,34 +2,18 @@ class Solution {
 public:
     bool checkPossibility(vector<int>& nums) {
         int n=nums.size();
-        bool ans=true;
-        vector<int>temp=nums;
         for(int i=0;i<n-1;i++){
-            if(temp[i] > temp[i+1]){
-                temp[i]=temp[i+1];
+            if(nums[i]>nums[i+1]){
+                if(i-1>=0 && nums[i-1]>nums[i+1]){
+                    nums[i+1]=nums[i];
+                } else {
+                    nums[i]=nums[i+1];
+                }
                 break;
             }
         }
         for(int i=0;i<n-1;i++){
-            if(temp[i] > temp[i+1]){
-                ans=false;
-                break;
-            }
-        }
-        for(auto x:temp) cout<<x<<" ";
-        if(ans) return true;
-        temp=nums;
-        cout<<endl;
-        for(int i=0;i<n-1;i++){
-            if(temp[i] > temp[i+1]){
-                temp[i+1]=temp[i];
-                break;
-            }
-        }
-        for(auto x:temp) cout<<x<<" ";
-        cout<<endl;
-        for(int i=0;i<n-1;i++){
-            if(temp[i] > temp[i+1]){
+            if(nums[i]>nums[i+1]){
                 return false;
             }
         }
