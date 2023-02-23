2319a1a91a50a46c0e88508609f02ad966ee69e8
Sumit2595 <72004520+Sumit2595@users.noreply.github.com>
2023-02-23 10:23:14 +0530

    Time: 394 ms (15.27%), Space: 102.4 MB (5.16%) - LeetHub

---
+++ 0502-ipo/0502-ipo.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        int ans = 0;
+        if(profits.size() == 0 || capital.size() == 0)
+            return ans;
+        map<int,vector<int>> dict;
+        map<int,vector<int>>::iterator it;
+        int n=profits.size();
+        for(int i=0;i<n;i++){
+            dict[capital[i]].push_back(profits[i]);
+        }
+        priority_queue<int>pq;
+        it=dict.begin();
+        while(k){
+            for(;it!=dict.end();++it){
+                if(it->first > w)
+                    break;
+                vector<int>temp=it->second;
+                for(int i=0;i<temp.size();i++){
+                    pq.push(temp[i]);
+                }
+            }
+            if(!pq.empty()){
+                w+=pq.top();pq.pop();
+                k--;
+            } else {
+                break;
+            }
+        }
+        return w;
+    }
+};
