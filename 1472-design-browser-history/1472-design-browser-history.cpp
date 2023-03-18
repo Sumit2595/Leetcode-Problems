6db8df8e2133536526647fb09ac9609bd6cb3961
Sumit2595 <72004520+Sumit2595@users.noreply.github.com>
2023-03-18 11:07:55 +0530

    Time: 147 ms (58.70%), Space: 57.6 MB (76.70%) - LeetHub

---
+++ 1472-design-browser-history/1472-design-browser-history.cpp
@@ -0,0 +1,32 @@
+class BrowserHistory {
+public:
+    vector<string>history;
+    int curr=0;
+    BrowserHistory(string homepage) {
+        history.push_back(homepage);
+    }
+    
+    void visit(string url) {
+        history.erase(history.begin()+curr+1,history.end());
+        history.push_back(url);
+        curr++;
+    }
+    
+    string back(int steps) {
+        curr= max(0, curr - steps);
+        return history[curr];
+    }
+    
+    string forward(int steps) {
+        curr=min((int)history.size()-1,curr+steps);
+        return history[curr];
+    }
+};
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory* obj = new BrowserHistory(homepage);
+ * obj->visit(url);
+ * string param_2 = obj->back(steps);
+ * string param_3 = obj->forward(steps);
+ */
