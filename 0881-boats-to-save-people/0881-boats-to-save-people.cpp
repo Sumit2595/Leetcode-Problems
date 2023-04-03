5d537e8fa400f1647cef4ac5031ca35cdc7c15b1
Sumit2595 <72004520+Sumit2595@users.noreply.github.com>
2023-04-03 08:41:25 +0530

    Time: 97 ms (21.88%), Space: 42 MB (11.29%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-    int numRescueBoats(vector<int>& nums, int limit) {
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-        int sum=nums[0];
-        int boats=0;
-        int low=0,high=n-1;
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        int low=0,high=people.size()-1;
+        int ans=0;
         while(low<=high){
-            int load=nums[low]+nums[high];
-            if(load<=limit){
-                boats++;
-                low++;
-                high--;
+            if(low!=high && people[low]+people[high] <= limit){
+                low++;high--;
             } else {
-             boats++;
                 high--;
             }
+            ans++;
         }
-        return boats;
+        return ans;
     }
 };
