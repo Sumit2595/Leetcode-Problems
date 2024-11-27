e3cc92a7dac2413cbd20ab5604050b312316f277
Sumit2595 <72004520+Sumit2595@users.noreply.github.com>
2024-11-27 23:31:39 +0530

    Time: 7 ms (10.20%), Space: 21.9 MB (5.94%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& nums, vector<int>& newInterval) {
-        vector<int>temp;
-        for(auto x:nums){
-            temp.push_back(x[0]);
-        }
-        int idx=lower_bound(temp.begin(),temp.end(),newInterval[0])-temp.begin();
-        nums.insert(nums.begin()+idx,newInterval);
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-        vector<vector<int>>ans;
-        int start=nums[0][0],end=nums[0][1];
-        for(int i=1;i<n;i++){
-            if(end>=nums[i][0] && end<=nums[i][1] || end>nums[i][1]){
-                end=max(end,nums[i][1]);
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        intervals.push_back(newInterval);
+        int n=intervals.size();
+        sort(intervals.begin(),intervals.end());
+        int start=intervals[0][0];
+        int end=intervals[0][1];
+        int j=1;
+        int i=0;
+        while(j<n){
+           if(intervals[i][1]>=intervals[j][0]){
+               intervals[i][1]=max(intervals[j][1],intervals[i][1]);
             } else {
-                ans.push_back({start,end});
-                start=nums[i][0];
-                end=nums[i][1];
-            }
+               i++;
+               intervals[i]=intervals[j];
+           }
+            j++;
         }
-        ans.push_back({start,end});
-        return ans;
+        intervals.erase(intervals.begin()+i+1,intervals.end());
+        return intervals;
     }
 };
