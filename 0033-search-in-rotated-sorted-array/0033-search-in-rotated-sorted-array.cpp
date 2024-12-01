19fb0343474ab3e9a3d69aebbd73a8d921feee33
Sumit2595 <72004520+Sumit2595@users.noreply.github.com>
2024-12-01 13:10:03 +0530

    Time: 0 ms (100.00%), Space: 15.2 MB (5.30%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
-    int binarySearch(vector<int>nums,int l,int r,int target){
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(nums[mid]>target){
-                r=mid-1;
-            } else if(nums[mid]<target){
-                l=mid+1;
-            } else {
+    int bsearch(vector<int>& nums,int low,int high,int target){
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid] == target)
                 return mid;
-            }
+            if(nums[mid]>target)
+                high=mid-1;
+            else
+                low=mid+1;
         }
         return -1;
     }
     int search(vector<int>& nums, int target) {
         int n=nums.size();
-        int low=0,high=n-1;
-        while(low<high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]>nums[high])
-                low=mid+1;
-            else
-                high=mid;
-        }
+       int low=0,high=n-1;
+       while(low<high){
+           int mid=low+(high-low)/2;
+           if(nums[mid]>nums[high])
+               low=mid+1;
+           else
+               high=mid;
+       }
         int pivot=low;
-        int left=binarySearch(nums,0,pivot-1,target);
-        int right=binarySearch(nums,pivot,n-1,target);
-        if(left!=-1){
+        int left=bsearch(nums,0,pivot-1,target);
+        int right=bsearch(nums,pivot,n-1,target);
+        if(left!=-1)
             return left;
-        } 
         if(right!=-1)
             return right;
         return -1;
