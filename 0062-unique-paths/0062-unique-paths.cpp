4b238c9cb3f99b8d3465e51e887e3054115a0721
Sumit2595 <72004520+Sumit2595@users.noreply.github.com>
2024-12-04 00:25:13 +0530

    Time: 6 ms (16.48%), Space: 12.2 MB (5.71%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int dp[1001][1001];
-    int solve(int i,int j,int m,int n){
-        if(i>=m || j>=n)
+    int solve(int x,int y,int m,int n){
+        if(x>=m || y>=n)
             return 0;
-        if(i==m-1 && j==n-1)
+        if(x==m-1 && y == n-1)
             return 1;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        int op1=solve(i+1,j,m,n);
-        int op2=solve(i,j+1,m,n);
-        return dp[i][j]=op1+op2;
+        if(dp[x][y]!=-1)
+            return dp[x][y];
+        return dp[x][y]=solve(x+1,y,m,n)+solve(x,y+1,m,n);
+        
     }
     int uniquePaths(int m, int n) {
         memset(dp,-1,sizeof(dp));
